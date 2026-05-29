7f3292feda1f05f744031712f7b8ba74103d349a
kirti <kirti@123>
2026-05-29 22:14:18 +0530

    Commit

--- Script1.sh
+++ Script1.sh
@@ -2,4 +2,6 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-33 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Git"
-echo "I am creating branch INC1311"
+echo "This is Feature branch Nikita"
+echo "We are doing conflict merge."
+
